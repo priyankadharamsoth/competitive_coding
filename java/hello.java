36296c417786e2dc70e6a12ff1c75abc82a1c228
priyanka dharamsoth <priyanka.d17@iiits.in>
2022-02-26 13:58:53 +0530

    segregate python and java codes

---
+++ java/hello.java
@@ -0,0 +1,18 @@
+class A{
+    public  void eat(Integer i){
+        System.out.println(i);
+    }
+
+
+
+   
+    
+
+}
+
+public class hello{  
+    public static void main(String args[]){
+        
+       
+    }
+}  
