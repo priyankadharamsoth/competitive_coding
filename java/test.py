41f9c5d4df394a3d4e9c3713af879dc18db58a6d
priyanka dharamsoth <priyanka.d17@iiits.in>
2022-03-22 21:45:33 +0530

    leetcode problems

--- java/test.py
+++ java/test.py
@@ -1,21 +1,70 @@
-from collections import Counter
+''' Python3 program to Count
+	minimum swap to make
+	string palindrome'''
+
+# Function to Count minimum swap
 
 
-s = 'rat'
-t = 'cat'
 
-hash_map_s = {}
-hash_map_t = {}
 
-for i in s:
-    if i not in hash_map_s.items():
-        hash_map_s[i] = 1
-    else:
-        hash_map_s[i] += 1
-for i in t:
-    if i not in hash_map_t.items():
-        hash_map_t[i] = 1
-    else:
-        hash_map_t[i] += 1
 
-print(hash_map_s == hash_map_t)
+def CountSwap(s, n):
+	s = list(s)
+
+	# Counter to count minimum swap
+	count = 0
+	ans = True
+
+	# A loop which run in half string
+	# from starting
+	for i in range(n // 2):
+
+		# Left pointer
+		left = i
+
+		# Right pointer
+		right = n - left - 1
+
+		# A loop which run from right pointer
+		# to left pointer
+		while left < right:
+
+			# if both char same then
+			# break the loop if not
+			# same then we have to move
+			# right pointer to one step left
+			if s[left] == s[right]:
+				break
+			else:
+				right -= 1                     
+                                               #1234123
+                                               #count = 1
+		# it denotes both pointer at
+		# same position and we don't
+		# have sufficient char to make
+		# palindrome string
+		if left == right:
+			ans = False
+			break
+		else:
+			for j in range(right, n - left - 1):
+				(s[j], s[j + 1]) = (s[j + 1], s[j])
+				count += 1
+            
+           
+	if ans:
+		return (count)
+	else:
+		return -1
+
+
+# Driver Code
+s = 'geeksfgeeks'
+
+# Length of string
+n = len(s)
+
+# Function calling
+ans1 = CountSwap(s, n)
+ans2 = CountSwap(s[::-1], n)
+print(max(ans1, ans2))
